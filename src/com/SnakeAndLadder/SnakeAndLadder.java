ac3d1ac5c6f4d5fc36cd17588e268d298d6728bf
pankaj kore <korep95@gmail.com>
2022-01-21 13:46:27 +0530

    [pankaj]added initial position of players

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -2,6 +2,8 @@ package com.SnakeAndLadder;
 
 public class SnakeAndLadder {
 		public static void main (String []args) {
+			int initialPosition = 0;
 			System.out.println("**welcome to the Snake And Ladder Game**");
 		}
 }
+
