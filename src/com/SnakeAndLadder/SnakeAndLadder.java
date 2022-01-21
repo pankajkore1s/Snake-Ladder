28268384a364ec721b139f11de26cc7ff5721af5
pankaj kore <korep95@gmail.com>
2022-01-21 14:00:55 +0530

    [pankaj]added (RANDOM)to generate the dice number

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -1,9 +1,16 @@
 package com.SnakeAndLadder;
 
+import java.util.*;
+
 public class SnakeAndLadder {
-		public static void main (String []args) {
+		
+	public static void main (String []args) {
+	
 			int initialPosition = 0;
-			System.out.println("**welcome to the Snake And Ladder Game**");
+			Random random=new Random();
+			int Dice=random.nextInt(6)+1;
+			System.out.println(Dice);
+		
 		}
 }
 
